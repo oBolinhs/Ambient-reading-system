ccf096118289c02ca8f9773c1bb81065f6055710
Bernardo Francisco <bernardo.rodriguesfrancisco@gmail.com>
2026-05-20 13:53:17 +0100

    Untracked file

--- src/sensitive_info.h
+++
@@ -1,8 +0,0 @@
-#define API_KEY "AIzaSyDR8ACcwzVig4FV_UKU-ZqUbaBMMpgfvFU"
-#define DATABASE_URL "esp-projects-44cdb-default-rtdb.europe-west1.firebasedatabase.app"
-
-#define USER_EMAIL "esp32.ambient@gmail.com"
-#define USER_PASSWORD "dht_123456"
-
-#define WIFI_SSID "Burger King - Guest"
-#define WIFI_PASSWORD "VinhoVerde"
